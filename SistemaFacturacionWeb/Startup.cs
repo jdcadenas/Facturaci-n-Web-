f99620aedfc4810c5bcd9ba25b38b06f2d27619a
black scorpion <Iconceal@hotmail.com>
2018-05-11 09:40:12 -0500

    Agregar archivos de proyecto.

---
+++ SistemaFacturacionWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaFacturacionWeb.Startup))]
+namespace SistemaFacturacionWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
